0a631e1af23222447fedd3f271c116a31cb7639d
adrian moreno perez <adrian.flda@gmail.com>
2019-10-16 04:26:04 -0400

    test

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const Flectra = require('./flectra')
-new Flectra()
+'use strict';
+require('dotenv').config()
+const Flectra = require('../flectra');
+
+const deployData = {
+    "url": process.env.URL,
+    "port": process.env.PORT,
+    "db": process.env.DB,
+    "username": process.env.USER_NAME,
+    "password": process.env.PASSWORD
+}
+
+
+let main = new Flectra(deployData)
+console.log(main)
+let leads = await main.readElement('res.partner', [['name', 'like', 'test']]);
+console.log(leads.length)
