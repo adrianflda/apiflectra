ed3d40693d86d9f341b09588abd90dd7767fe45d
adrian moreno perez <adrian.flda@gmail.com>
2019-10-16 09:56:43 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
 require('dotenv').config()
 const Flectra = require('./flectra');
+const { Sema } = require('async-sema');
+const s = new Sema(
+    4, // Allow 4 concurrent async calls
+    {
+        capacity: 1000 // Prealloc space for 100 tokens
+    }
+);
 
 const deployData = {
     "url": process.env.URL,
@@ -10,23 +17,23 @@ const deployData = {
     "password": process.env.PASSWORD
 }
 
-
-
 const main = async () => {
     try {
         let main = new Flectra(deployData)
         await main.connect()
         //let team = await main.readElement('crm.team', [['name', '=', 'Parejeros-MTY-TP']])
-       // let agents = team && team.x_agent_ids
+        // let agents = team && team.x_agent_ids
         let leads = await main.readElement('crm.lead', [['user_id', '=', 18]]);
         console.log('elements: ', leads.length)
         leads.forEach(async element => {
+            await s.acquire()
             let newLead = {
                 id: element.id,
                 user_id: 80,
                 team_id: 118
             }
-            await main.updateElement('crm.lead',newLead)
+            await main.updateElement('crm.lead', newLead)
+            await s.release()
         });
     } catch (error) {
         console.log('main error: ', error)
