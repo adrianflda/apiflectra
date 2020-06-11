6095e05cb3ef80528c43350cf8ca52f269c8487a
adrian moreno perez <adrian.flda@gmail.com>
2020-06-11 16:02:03 -0400

    update lemenets

--- flectra.js
+++ flectra.js
@@ -1,233 +1,206 @@
-const Odoo = require('odoo-xmlrpc')
-require('dotenv').config()
+const Odoo = require("odoo-xmlrpc");
+require("dotenv").config();
 
+const { Sema } = require("async-sema");
+const sema = new Sema(
+  1, // Allow 4 concurrent async calls
+  { capacity: 10000 } // Prealloc space for 1000 tokens
+);
 
 // methods
-const CREATE = 'create'
-const SEARCH_READ = 'search_read'
-const WRITE = 'write'
-const UNLINK = 'unlink'
+const CREATE = "create";
+const SEARCH_READ = "search_read";
+const WRITE = "write";
+const UNLINK = "unlink";
 
 /**
  * Deploy data url, port, db, username, passowrd
  */
 const loadDeployData = {
-    "url": process.env.URL,
-    "port": process.env.PORT,
-    "db": process.env.DB,
-    "username": process.env.USER_NAME,
-    "password": process.env.PASSWORD
-}
+  url: process.env.URL,
+  port: process.env.PORT,
+  db: process.env.DB,
+  username: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+};
 
 class Flectra {
-    constructor(deployData, evenListener) {
-        this.deployData = deployData
+  constructor(deployData, evenListener) {
+    this.deployData = deployData;
+  }
 
-        if (evenListener) {
-            const cote = require('cote');
-            const listener = new cote.Responder({ name: 'API Flectra Listener', envairoment: 'production', key: 'flectra' });
-
-            listener.on('createElement', async (req = {}, cb) => {
-                console.log('apiflectra createElement: ', req)
-                let { context = {}, model, element } = req
-                let result = await this.createElement(context, model, element)
-                cb(null, result)
-            });
-
-            listener.on('readElement', async (req = {}, cb) => {
-                console.log('apiflectra readElement: ', req)
-                let { model, filter, fields, offset, limit } = req
-                let result = await this.readElement(model, filter, fields, offset, limit)
-                cb(null, result)
-            });
-
-            listener.on('updateElement', async (req = {}, cb) => {
-                console.log('apiflectra updateElement: ', req)
-                let { model, element } = req
-                let result = await this.updateElement(model, element)
-                cb(null, result)
-            });
-
-            listener.on('deleteElement', async (req = {}, cb) => {
-                console.log('apiflectra deleteElement: ', req)
-                let { model, id } = req
-                let result = await this.deleteElement(model, id)
-                cb(null, result)
-            });
+  /**
+   *
+   * @param {Object} deployData deploy parameters
+   */
+  connect(deployData = loadDeployData) {
+    console.log("connecting to flcetra", deployData);
+    return new Promise((resolve, reject) => {
+      try {
+        if (this.flectra) {
+          resolve(this.flectra);
+          return;
         }
-    }
+        this.flectra = new Odoo(deployData);
+        this.flectra.connect((err, value) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          console.log("user_id: ", value);
+          resolve(this.flectra);
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
+  /**
+   *
+   * @param {String} model Flectra model
+   * @param {String} method Flectra method
+   * @param {Array} params Parameters for each job
+   */
+  execute_kw(model, method, params) {
+    return new Promise(async (resolve, reject) => {
+      if (this.flectra) {
+        console.log("execute_kw: ", model, method, JSON.stringify(params));
+        await sema.acquire();
+        this.flectra.execute_kw(model, method, params, async (err, value) => {
+          sema.release();
+          if (err) {
+            console.log(
+              "execute_kw fail: ",
+              model,
+              method,
+              JSON.stringify(params),
+              err
+            );
+            reject(err);
+            return;
+          }
+          resolve(value);
+        });
+        return;
+      } else reject("flectra is not initialized");
+    });
+  }
 
-    /**
-     * 
-     * @param {Object} deployData deploy parameters
-     */
-    connect(deployData = loadDeployData) {
-        console.log('connecting to flcetra', deployData)
-        return new Promise((resolve, reject) => {
-            try {
-                if (this.flectra) {
-                    resolve(this.flectra)
-                    return
-                }
-                this.flectra = new Odoo(deployData)
-                this.flectra.connect((err, value) => {
-                    if (err) {
-                        reject(err)
-                        return
-                    }
-                    console.log('user_id: ', value)
-                    resolve(this.flectra);
-                })
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
+  /**
+   *
+   * @param {Object} context Context for flectra enviroment
+   * @param {String} model Flectra model
+   * @param {Object} element Specific element to create
+   */
+  createElement(context, model, element) {
+    return new Promise((resolve, reject) => {
+      if (!model || !element) {
+        console.log("trace", "createElement", [model, element]);
+        reject("not element");
+        return;
+      }
 
-    /**
-     * 
-     * @param {String} model Flectra model 
-     * @param {String} method Flectra method 
-     * @param {Array} params Parameters for each job 
-     */
-    execute_kw(model, method, params) {
-        return new Promise((resolve, reject) => {
-            if (this.flectra) {
-                console.log('execute_kw: ', model, method, JSON.stringify(params))
-                this.flectra.execute_kw(model, method, params, (err, value) => {
-                    if (err) {
-                        console.log('execute_kw fail: ', model, method, JSON.stringify(params), err)
-                        reject(err)
-                        return
-                    }
-                    resolve(value)
-                })
-                return
-            } else
-                reject('flectra is not initialized')
-        })
-    }
+      context = context || {};
+      let inParams = [];
+      inParams.push(element);
+      inParams.push(context);
+      let params = [];
+      params.push(inParams);
 
-    /**
-     * 
-     * @param {Object} context Context for flectra enviroment 
-     * @param {String} model Flectra model 
-     * @param {Object} element Specific element to create
-     */
-    createElement(context, model, element) {
-        return new Promise((resolve, reject) => {
-            if (!model || !element) {
-                console.log('trace', 'createElement', [model, element])
-                reject('not element')
-                return
-            }
+      this.execute_kw(model, CREATE, params)
+        .then((result) => resolve(result))
+        .catch((e) => reject(e));
+    });
+  }
 
-            context = context || {}
-            let inParams = [];
-            inParams.push(element);
-            inParams.push(context);
-            let params = [];
-            params.push(inParams);
+  /**
+   *
+   * @param {String} model Flectra model
+   * @param {Object} element Specific element to update
+   */
+  updateElement(model, element) {
+    return new Promise((resolve, reject) => {
+      if (!model || !element || !element.id) {
+        console.log("error", "updateElement", [model, element]);
+        reject("no element");
+        return;
+      }
 
-            this.execute_kw(model, CREATE, params)
-                .then(result => resolve(result))
-                .catch(e => reject(e))
+      let id = element.id;
+      delete element.id;
 
-        })
-    }
+      let inParams = [];
+      inParams.push([id]);
+      inParams.push(element);
+      let params = [];
+      params.push(inParams);
 
-    /**
-     * 
-     * @param {String} model Flectra model
-     * @param {Object} element Specific element to update
-     */
-    updateElement(model, element) {
-        return new Promise((resolve, reject) => {
-            if (!model || !element || !element.id) {
-                console.log('error', 'updateElement', [model, element])
-                reject('no element')
-                return
-            }
+      this.execute_kw(model, WRITE, params)
+        .then((result) => resolve(result))
+        .catch((e) => reject(e));
+    });
+  }
 
-            let id = element.id
-            delete element.id
+  /**
+   *
+   * @param {String} model Flectra model
+   * @param {Array} filter Filter
+   * @param {Array} fields Fields
+   * @param {Number} offset Offset
+   * @param {Number} limit Limit
+   */
+  readElement(model, filter, fields, offset, limit) {
+    filter = filter || [];
+    fields = fields || 0;
+    limit = limit || 0;
+    offset = offset || 0;
 
-            let inParams = [];
-            inParams.push([id]);
-            inParams.push(element)
-            let params = [];
-            params.push(inParams);
-
-            this.execute_kw(model, WRITE, params)
-                .then(result => resolve(result))
-                .catch(e => reject(e))
-
-        })
-    }
-
-
-    /**
-     * 
-     * @param {String} model Flectra model
-     * @param {Array} filter Filter
-     * @param {Array} fields Fields
-     * @param {Number} offset Offset
-     * @param {Number} limit Limit
-     */
-    readElement(model, filter, fields, offset, limit) {
-        filter = filter || []
-        fields = fields || 0
-        limit = limit || 0
-        offset = offset || 0
-
-        return new Promise(async (resolve, reject) => {
-            try {
-                var inParams = [];
-                inParams.push(filter)
-                inParams.push(fields)
-                inParams.push(offset)
-                inParams.push(limit)
-                var params = []
-                params.push(inParams)
-
-                let result = await this.execute_kw(model, SEARCH_READ, params)
-
-                if (fields) {
-                    let array = []
-                    for (let elementIndex in result) {
-                        let element = result[elementIndex]
-                        let newElement = {}
-                        for (let fieldIndex in fields) {
-                            let field = fields[fieldIndex]
-                            newElement[field] = element[field]
-                        }
-                        array.push(newElement)
-                    }
-                    result = array
-                }
-                result = (limit === 1) ? result && result[0] : result
-                resolve(result)
-
-            } catch (error) {
-                console.log('error', 'readElement', error)
-                reject(error)
-            }
-
-        })
-    }
-
-    deleteElement(model, id) {
-        let inParams = [];
-        inParams.push([id]); //id to delete
-        let params = [];
+    return new Promise(async (resolve, reject) => {
+      try {
+        var inParams = [];
+        inParams.push(filter);
+        inParams.push(fields);
+        inParams.push(offset);
+        inParams.push(limit);
+        var params = [];
         params.push(inParams);
-        return new Promise((resolve, reject) => {
-            this.execute_kw(model, UNLINK, params)
-                .then(result => resolve(result))
-                .catch(e => reject(e))
-        })
-    }
+
+        let result = await this.execute_kw(model, SEARCH_READ, params);
+
+        if (fields) {
+          let array = [];
+          for (let elementIndex in result) {
+            let element = result[elementIndex];
+            let newElement = {};
+            for (let fieldIndex in fields) {
+              let field = fields[fieldIndex];
+              newElement[field] = element[field];
+            }
+            array.push(newElement);
+          }
+          result = array;
+        }
+        result = limit === 1 ? result && result[0] : result;
+        resolve(result);
+      } catch (error) {
+        console.log("error", "readElement", error);
+        reject(error);
+      }
+    });
+  }
+
+  deleteElement(model, id) {
+    let inParams = [];
+    inParams.push([id]); //id to delete
+    let params = [];
+    params.push(inParams);
+    return new Promise((resolve, reject) => {
+      this.execute_kw(model, UNLINK, params)
+        .then((result) => resolve(result))
+        .catch((e) => reject(e));
+    });
+  }
 }
 
-module.exports = Flectra
+module.exports = Flectra;
--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const { processXLSXToLeads, updateLeadFields, workWithThis } = require('./api')
-const crm_name = 'B2C'
-const country_name = 'Mexico'
-const state_name = ''
-const agent_login = 'erik.cruz@vacancyrewards.com'
+const { processXLSXToLeads, updateLeadFields, workWithThis } = require("./api");
+const crm_name = "B2C";
+const country_name = "Mexico";
+const state_name = "";
+const agent_login = "erik.cruz@vacancyrewards.com";
 
-processXLSXToLeads({
-    crm_name,
-    country_name, 
-    state_name, 
-    agent_login })
+// processXLSXToLeads({ crm_name, country_name, state_name, agent_login });
 
 //API.workWithThis('crm.lead', [['stage', '=', false]], updateLeadFields) //for update some fields valuess
+
+const Flectra = require("./flectra");
+const main = new Flectra();
+
+const changeName = (element) => {
+  const regex = /-MTY/gi;
+  element.name = element.name.replace(regex, "");
+  return element;
+};
+
+const updateElement = async (model, element) => {
+  await main.updateElement(model, element);
+};
+
+const updateElements = async (model, elements, doSomeThing) => {
+  let index = 0;
+  for (let element of elements) {
+    await doSomeThing(element);
+    await updateElement(model, element);
+    console.log(index++, elements.length - index);
+  }
+};
+
+const search = async (model, filter, fields, limit) => {
+  return await main.readElement(model, filter, fields, 0, limit);
+};
+
+const init = async () => {
+  await main.connect();
+};
+
+(async () => {
+  await init();
+  const model = "crm.lead";
+  const filter = [["name", "ilike", "agentleads-mty-monterrey"]];
+  const fields = ["id", "name"];
+
+  const elements = await search("crm.lead", filter, fields);
+  console.log(elements && elements.length);
+
+  await updateElements(model, elements, changeName);
+})();
